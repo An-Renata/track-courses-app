e5818e6ccb443d0dbe0eb3a2a893f7712a969e2f
Renata <110370964+An-Renata@users.noreply.github.com>
2023-10-08 14:31:33 +0300

    Building static HTML markup
    Structuring layout, before adding interactivity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+const courseList = [
+  {
+    id: 1,
+    name: "The complete JavaScript Course 2023: From Zero to Expert!",
+    instructor: "Jonas Schmedtmann",
+    hours: 69,
+    mainFocus: "Javascript",
+  },
+  {
+    id: 2,
+    name: "JavaScript Alhorithms and Data Structures",
+    instructor: "FreeCodeCamp",
+    hours: 300,
+    mainFocus: "Javascript",
+  },
+  {
+    id: 3,
+    name: "Build Responsive Real-World Websites with HTML and CSS",
+    instructor: "Jonas Schmedtmann",
+    hours: 37.5,
+    mainFocus: "HTML + CSS",
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>Courses tracker</h1>
+      <div className="container">
+        <AsideBar />
+        <Courses />
+        <AddForm />
+      </div>
+    </>
+  );
+}
+
+function AsideBar() {
+  return (
+    <div className="aside">
+      <button className="add-btn">Add +</button>
+      <div>
+        <ul>
+          <li>React</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Courses() {
+  return (
+    <div className="course-window">
+      <ul>
+        {courseList.map((course) => (
+          <li key={course.id}>
+            <div>
+              <h3 className="course-name">{course.name}</h3>
+              <p>
+                <strong>Instructor/Source:</strong> {course.instructor}
+              </p>
+            </div>
+            <span className="hours">Hours: {course.hours}h</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function AddForm() {
+  return (
+    <div className="add-form">
+      <h3>Add a new course</h3>
+      <form>
+        <label>Name of the course</label>
+        <input type="text"></input>
+
+        <label>Instructor/Source of the course</label>
+        <input type="text"></input>
+
+        <label>Total hours</label>
+        <input type="text"></input>
+
+        <label>
+          Main focus on: <br></br>
+          <small>
+            (Describe it with one or to words e.g "React", "HTML + CSS")
+          </small>
+        </label>
+        <input type="text"></input>
+
+        <div class="form-btn">
+          <button>Close</button>
+          <button>Add</button>
+        </div>
+      </form>
+    </div>
+  );
+}
